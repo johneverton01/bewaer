8fa419ff6b7261931c9e6e92783c4beadab1d57a
John Everton <johneverton44@gmail.com>
2025-08-06 22:29:53 -0300

    feat: fetch and display newly created products in the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import { CategorySelector } from "@/components/common/CategorySelector";
 import { Header } from "@/components/common/Header";
 import { ListProducts } from "@/components/common/ListProducts";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 export default async function Home() {
   const products = await db.query.productTable.findMany({
     with: {
       variants: true
     },
+  });
+
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    with: {
+      variants: true
+    },
+    orderBy: [desc(productTable.createdAt)],
   });
 
   const categories = await db.query.categoryTable.findMany();
@@ -45,6 +54,11 @@ export default async function Home() {
             className="h-auto w-full"
           />
         </div>
+
+        <ListProducts 
+          title="Novos produtos" 
+          products={newlyCreatedProducts} 
+        />
       </div>
     </div>
   );
